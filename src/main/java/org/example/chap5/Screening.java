1f4a7e4ca43e6b6555442d1bd0f56ec5bfebc020
Hyeonjun Park <phjppo0918@gmail.com>
2023-10-01 17:27:18 +0900

    chap5-146p: 영화 예매 메시지를 처리할 메서드 구현

---
+++ src/main/java/org/example/chap5/Screening.java
@@ -0,0 +1,13 @@
+package org.example.chap5;
+
+import org.example.chap2.Customer;
+import org.example.chap2.Money;
+import org.example.chap2.Movie;
+import org.example.chap2.Reservation;
+
+import java.time.LocalDateTime;
+
+public class Screening {
+    public Reservation reserve(Customer customer, int audienceCount) {
+    }
+}
