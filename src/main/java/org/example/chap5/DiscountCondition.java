fb96f5ba7016fa2ead141dd06bec9b1e4317271a
Hyeonjun Park <phjppo0918@gmail.com>
2023-10-01 17:44:30 +0900

    chap5-149: Movie 메서드 구현

---
+++ src/main/java/org/example/chap5/DiscountCondition.java
@@ -0,0 +1,5 @@
+package org.example.chap5;
+
+public interface DiscountCondition {
+    boolean isSatisfiedBy(Screening screening);
+}
--- src/main/java/org/example/chap5/Movie.java
+++ src/main/java/org/example/chap5/Movie.java
@@ -1,6 +1,5 @@
 package org.example.chap5;
 
-import org.example.chap2.DiscountCondition;
 import org.example.chap2.Money;
 
 import java.time.Duration;
@@ -17,5 +16,36 @@ public class Movie {
     private double discountPercent;
 
     public Money calculateMovieFee(Screening screening) {
+        if(isDiscountable(screening)) {
+            return fee.minus(calculateDiscountAmount());
+        }
+        return fee;
+    }
+
+    private Money calculateDiscountAmount() {
+        switch (movieType) {
+            case AMOUNT_DISCOUNT :
+                return calculateAmountDiscountAmount();
+            case PERCENT_DISCOUNT:
+                return calculatePercentDiscountAmount();
+            case NONE_DISCOUNT:
+                return calculateNoneDiscountAmount();
+        }
+
+        throw new IllegalStateException();
+    }
+    private Money calculateAmountDiscountAmount() {
+        return discountAmount;
+    }
+    private Money calculatePercentDiscountAmount() {
+        return fee.times(discountPercent);
+    }
+
+    private Money calculateNoneDiscountAmount() {
+        return Money.ZERO;
+    }
+
+    private boolean isDiscountable(Screening screening) {
+        return discountConditions.stream().anyMatch(condition -> condition.isSatisfiedBy(screening));
     }
 }
