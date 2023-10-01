9b8aecf85d39722050a9a24f04e1b40834594e95
Hyeonjun Park <phjppo0918@gmail.com>
2023-10-01 17:28:16 +0900

    chap5-146p: 예매 메시지를 수행하는데 필요한 인스턴스 추가

--- src/main/java/org/example/chap5/Screening.java
+++ src/main/java/org/example/chap5/Screening.java
@@ -8,6 +8,9 @@ import org.example.chap2.Reservation;
 import java.time.LocalDateTime;
 
 public class Screening {
+    private Movie movie;
+    private int sequence;
+    private LocalDateTime whenScreened;
     public Reservation reserve(Customer customer, int audienceCount) {
     }
 }
